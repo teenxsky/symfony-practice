ea48c34d367c3b47492d77df1351ccc7eebe4387
teenxsky <kutorgin2002@gmail.com>
2025-03-04 10:31:26 +1000

    Add base symfony project
    Update composer dependencies
    
    Add project infrastructure for development
    
    Update .gitignores
    
    Add directory for db dumbs
    
    Add Makefile
    
    Add admin panel
    
    Add multi-stage backend build and internal deps

---
+++ .docker/dev/php/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$DATABASE"="postgres" ]
+then
+    echo "Waiting for postgreSQL..."
+
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+        sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+exec "$@"
